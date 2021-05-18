c23db5441422218a05c74ab747fc1acde7117e47
junior-mario <mario.junior@live.com>
2021-05-18 11:36:30 -0300

    Add files via upload

---
+++ PABX/ColetaE1Vivo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#########################################################################
+#                                                                       #
+# Coleta dos juntores da Embratel -Coleta Juntores                      #
+#                                                                       #
+#                                                                       #
+# Autor: Mario (email de contato)                                       #
+# Data de criação: 27.11.18                                             #
+#                                                                       #
+# Descrição : Coleta os juntores dos E1 da Embratel direto do PABX      #
+# e salva a saida em /tmp/E1_Embratel.txt                               #
+#                                                                       #
+#                                                                       #
+#                                                                       #
+# Exemplo de uso: ./Coleta_E1_Embratel.sh                               #
+#                                                                       #
+# Alterações                                                            #
+#                                                                       #
+#########################################################################
+
+(sleep 3; echo "mtcl";sleep 2; echo "baroneza3138"; sleep 3;echo "trkstat -r 0";sleep 3 ) | telnet 172.16.9.10 > /tmp/E1_Vivo.txt
